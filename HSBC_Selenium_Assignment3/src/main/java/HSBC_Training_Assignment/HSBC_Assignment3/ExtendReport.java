9f52078e3dc1f4229488edbfdf45125cffa92b1a
chippasujeethkumar <chippasujeethkumar@gmail.com>
2020-11-08 04:02:10 +0530

    Submission

--- HSBC_Selenium_Assignment3/src/main/java/HSBC_Training_Assignment/HSBC_Assignment3/ExtendReport.java
+++ HSBC_Selenium_Assignment3/src/main/java/HSBC_Training_Assignment/HSBC_Assignment3/ExtendReport.java
@@ -1,34 +1,35 @@
 package HSBC_Training_Assignment.HSBC_Assignment3;
 
-import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import pricegrabber.pricegrabber.ExtentHtmlReporter;
-import pricegrabber.pricegrabber.ExtentReports;
-import pricegrabber.pricegrabber.ExtentTest;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 public class ExtendReport 
 {
-	public class  ExtendReport
-	{
-		@Test
-		public void loginTest()
+	public static void main(String[] args)
 		{
-			ExtentHtmlReporter reporter=new ExtentHtmlReporter("D:\\HSBC_Final\\extendreport.html");
-			
-			
+			ExtentHtmlReporter reporter=new ExtentHtmlReporter("D:\\HSBC\\HSBC_Final\\extendreport.html");			
 			ExtentReports extent=new ExtentReports();
-			
 			extent.attachReporter(reporter);
+			ExtentTest test1=extent.createTest("Google search test","test to validate google functionality");
 			
-			ExtentTest logger1=extent.createTest("LoginTest");
-			logger1.log(Status.INFO, "LOGIN TO HomePage");
-			logger1.log(Status.PASS, "Title verified");
-			extent.flush();
-			
-			ExtentTest logger2=extent.createTest("Logoff Test");
-			logger2.log(Status.INFO, "Title to be Verified");
-			logger2.log(Status.FAIL, "Title verified");
-			extent.flush();		
+			System.setProperty("webdriver.chrome.driver","D:\\Selenium jar files\\chrome86\\chromedriver.exe");
+		    WebDriver driver = new ChromeDriver();
+		    driver.manage().window().maximize();
+		    driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		    
+		    test1.log(Status.INFO, "Starting testcase");
+		    driver.get("http://www.google.com");
+		    test1.log(Status.PASS, "Navigating to google page");
+		    test1.info("Test completed");
+		    extent.flush();
+		    
+		
 		}
-	}
+	
 }
